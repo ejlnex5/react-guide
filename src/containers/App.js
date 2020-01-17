3b7abe789a293876c48558c79e28aa4f552074bb
root <root@DESKTOP-F5UITTA.localdomain>
2020-01-16 17:23:15 -0800

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import classes from './App.module.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
+
+class App extends Component{
+  state = {
+    persons: [
+      {id: 'asdf', name: "Jeff", age: 32},
+      {id: 'zxcv', name: "Maria", age: 27},
+      {id: 'qree', name: "John", age: 30}
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({
+      persons: persons
+    })
+  }
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons.slice();
+    //const persons = [...this.state.persons];
+    //Equivalent to the first copy method.
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  render(){
+
+  let persons = null;
+
+  if ( this.state.showPersons ) {
+    persons = <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />;
+  }
+
+  return (
+    <div className={classes.App}>
+      <Cockpit 
+        showPersons={this.state.showPersons}
+        persons={this.state.persons}
+        clicked={this.togglePersonHandler} />
+      {persons}
+    </div>
+  );
+  }
+}
+
+export default App;
