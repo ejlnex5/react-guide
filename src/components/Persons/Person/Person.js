e4aebbf1c840e6e84ef53bfd2ebbf3ef2756aa21
ejlnex5 <ejlingo@gmail.com>
2020-01-25 12:34:32 -0800

    Implemented wrapper component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import classes from './Person.module.css';
+import Aux from '../../../hoc/Aux';
 
 
 class Person extends Component {
     render() {
     console.log('[Person.js] rendering...');
-    return (
-        <div className={classes.Person} >
-            <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age}!</p>
-            <p>{this.props.children}</p>
-            <input type='text' onChange={this.props.changed} value={this.props.name}/>
-        </div>
-    )
+    return(
+    <Aux>
+        <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age}!</p>
+        <p key="i2">{this.props.children}</p>
+        <input key="i3" type='text' onChange={this.props.changed} value={this.props.name}/>
+    </Aux>)
     }
 };
 
